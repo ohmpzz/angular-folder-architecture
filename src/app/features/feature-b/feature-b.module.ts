e1ef24bd702b5aa1d5d0468631d2b6463bb02c04
Ohm Piromrak <ohmpzzz@gmail.com>
2018-06-03 03:11:16 +0700

    work for me : )

---
+++ src/app/features/feature-b/feature-b.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BComponent } from './components/b/b.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BComponent]
+})
+export class FeatureBModule { }
